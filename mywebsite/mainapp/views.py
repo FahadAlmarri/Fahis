6d009e1e2594ebde4fd54ff6e9434ce4300f673e
عبدالمجيد المشاري <abdulmajeed.almeshari@gmail.com>
2023-02-09 23:23:33 +0300

    fixing_backend

--- mywebsite/mainapp/views.py
+++ mywebsite/mainapp/views.py
@@ -15,67 +15,44 @@ def report(request,sampleID):
 	#report=getreport(reportID)
 	sample = Sample.objects.get(id=sampleID)
 	report=Report.objects.get(Report_ID=sample.ReportID)
-	
-	context={"sample":sample, "report":report}
-	return render(request,'frontend/result.html',context)
+	if sample.Privacy_Type== 'private':
+		if request.user == sample.UserID:
+			context={"sample":sample, "report":report}
+			return render(request,'frontend/result.html',context)
+		else:
+			return redirect('history')
+	else:
+		context={"sample":sample, "report":report}
+		return render(request,'frontend/result.html',context)
 
 def home(request):
-		if(request.method=='POST'):
-			privacy=request.POST.get('privacy')
-			if not (request.user.is_authenticated):
-				if privacy =="private":
-					return redirect('/members/login_user')
-			
-			uploaded_url=request.POST.get('url')
-			if uploaded_url == None or uploaded_url=="":
-				uploaded_file=request.FILES['file']
-				sampleID=uploadfile(uploaded_file,privacy,request)
-			else:
-				url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-
-
-			
-
-			
-			
-			
-
-				
-			#redirct to report
-			
-			return redirect(f"report/{sampleID}")
-		return render(request, 'frontend/index.html', {},)
+	if(request.method=='POST'):
+		privacy=request.POST.get('privacy')
+		if not (request.user.is_authenticated):
+			if privacy =="private":
+				return redirect('/members/login_user')
+		#url coming soon
+		# uploaded_url=request.POST.get('url')
+		# if uploaded_url == None or uploaded_url=="":
+		uploaded_file=request.FILES['file']
+		sampleID=uploadfile(uploaded_file,privacy,request)
+		# else:
+		# 	url = 'https://www.virustotal.com/vtapi/v2/url/scan'
+		# return redirect(f"report/{sampleID}")
+	return render(request, 'frontend/index.html', {},)
 
 
 def history(request): 
-	hash = request.POST.get("hash")
-	print(hash)
 	if not (request.user.is_authenticated):
-		if(hash!=None and hash!=""):
-			print(hash)
-			sample_list = Sample.objects.filter(Privacy_Type="public", Sample_Address=hash)
-			allReports = Report.objects.all()
-			print(sample_list)
-			return render(request,'frontend/history.html', {'sample_list': sample_list,'allReports':allReports})
-		else:
-			sample_list = Sample.objects.filter(Privacy_Type="public")
-			print(sample_list)
-			allReports = Report.objects.all()
-			return render(request,'frontend/history.html', {'sample_list': sample_list,'allReports':allReports})
+		sample_list = Sample.objects.filter(Privacy_Type="public")
+		print(sample_list)
+		return render(request,'frontend/history.html', {'sample_list': sample_list})
 	else:
-		if(hash!=None and hash!=""):
-			print(hash)
-			sample_list = Sample.objects.filter(Privacy_Type="public", Sample_Address=hash)
-			allReports = Report.objects.all()
-			user_list = Sample.objects.filter(UserID_id=request.user,Sample_Address=hash)
-			print(sample_list)
-			return render(request,'frontend/history.html', {'sample_list': sample_list,'allReports':allReports,"user_list":user_list})
-		else:
-			sample_list = Sample.objects.filter(Privacy_Type="public")
-			print(sample_list)
-			allReports = Report.objects.all()
-			user_list = Sample.objects.filter(UserID_id=request.user)
-			return render(request,'frontend/history.html', {'sample_list': sample_list,'allReports':allReports,"user_list":user_list})
+		sample_list = Sample.objects.filter(Privacy_Type="public")
+		print(sample_list)
+		user_list = Sample.objects.filter(UserID_id=request.user)
+		print(user_list)
+		return render(request,'frontend/history.html', {'sample_list': sample_list,"user_list":user_list})
 	
 
 def forgot(request):
