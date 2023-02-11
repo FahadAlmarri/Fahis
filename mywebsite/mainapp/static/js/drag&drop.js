3876892f3f590a7ceed4445cc22120b236b79eb8
YousefAlghusn <yousef321gh@gmail.com>
2023-02-11 13:00:11 +0300

    risk precentage

--- mywebsite/mainapp/static/js/drag&drop.js
+++ mywebsite/mainapp/static/js/drag&drop.js
@@ -5,7 +5,6 @@ const dragText = document.getElementById("drag-text");
 
 dragBox.addEventListener('dragover', (event)=>{
     event.preventDefault();
-    console.log("dragover");
     
     dragText.innerText = "افلت الملف للرفع";
     dragText.style.color = "#712dc8";
@@ -13,16 +12,13 @@ dragBox.addEventListener('dragover', (event)=>{
 })
 
 dragBox.addEventListener('dragleave', ()=>{
-    console.log("left");
     dragText.innerText = "اسحب الملف هنا";
     dragText.style.color = "black";
 })
 
 dragBox.addEventListener('drop', (event)=>{
     event.preventDefault();
-    console.log("dropped");
-    
+
     input.files = event.dataTransfer.files;
     input.onchange()
-    console.log(input.files[0]);
 })
